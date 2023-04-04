9cdb7e31bd304de9c778dce67f7fb1ac6763bf3a
Damien Dumontet <damien.dumontet@protonmail.com>
2023-04-03 21:04:26 -0300

    add contact expo lib

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, View } from "react-native";
 import { Colors } from "./constants/Colors";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
+import * as Contacts from "expo-contacts";
 
 export default function App() {
   dayjs.extend(utc);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.PhoneNumbers],
+        });
+
+        console.log(data);
+      }
+    })();
+  }, []);
 
   return (
     <View style={styles.container}>
