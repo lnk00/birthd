8cfbe8ded96152769084841fda6ff3806d9077f7
Damien Dumontet <damien.dumontet@protonmail.com>
2023-04-11 17:16:40 -0300

    display contact

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text, FlatList } from "react-native";
 import { Colors } from "./constants/Colors";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import * as Contacts from "expo-contacts";
 
+interface ItemProps {
+  contact: Contacts.Contact;
+}
+
+const Item = ({contact}: ItemProps) => {
+  return (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemText}>{contact.name}</Text>
+    </View>
+  )
+}
+
 export default function App() {
   dayjs.extend(utc);
+
+  const [contacts, setContact] = useState<Contacts.Contact[]>([]);
 
   useEffect(() => {
     (async () => {
@@ -17,7 +31,9 @@ export default function App() {
           fields: [Contacts.Fields.PhoneNumbers],
         });
 
-        console.log(data);
+        console.log(data)
+
+        setContact(data)
       }
     })();
   }, []);
@@ -25,6 +41,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <SafeAreaView>
+        <Text style={styles.title}>Contacts</Text>
+        <FlatList style={styles.list} data={contacts} renderItem={({item}) => <Item contact={item} />} />
       </SafeAreaView>
       <StatusBar style="light" />
     </View>
@@ -38,4 +56,24 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "space-between",
   },
+  title: {
+    color: Colors.white,
+    fontWeight: "800",
+    fontSize: 24,
+    marginLeft: 18,
+    marginTop: 12
+  }, 
+  list: {
+    marginHorizontal: 18,
+    marginTop: 12
+  },
+  itemContainer: {
+    marginTop: 12,
+    paddingBottom: 12,
+    borderBottomColor: Colors.darkGrey,
+    borderBottomWidth: 1
+  },
+  itemText: {
+    color: Colors.white,
+  }
 });
